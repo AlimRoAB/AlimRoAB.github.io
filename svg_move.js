22c1aec8d279aaf1134b1a105e03f76fc1b1636e
AlimRoAB <kabaca.alim@gmail.com>
2017-11-15 08:50:47 -0800

    svg move example

---
+++ svg_move.js
@@ -0,0 +1,21 @@
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.0"; 
+
+
+var circle = document.getElementById("id_circle");
+var circle2 = document.getElementById("id_circle2");
+circle.addEventListener("touchmove", on_touch_move);
+circle2.addEventListener("touchmove", on_touch_move);
+var svg = document.getElementById("id_svg");
+var rect_svg = svg.getBoundingClientRect();
+
+function on_touch_move(e)
+{
+	var touches = e.changedTouches;
+
+	for (var i = 0; i < touches.length; i++){
+	var circle = touches[i].target;
+	circle.setAttribute("cx , touches[i].pageX - rect_svg.letf);
+	circle.setAttribute("cy , touches[i].pageX - rect_svg.top);
+	
+	}
+}
