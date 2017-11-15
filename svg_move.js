a5a8eef36184cdd344fb209a38f1c5da84bb8106
AlimRoAB <kabaca.alim@gmail.com>
2017-11-15 09:12:36 -0800

    cirle in side box

--- svg_move.js
+++ svg_move.js
@@ -1,4 +1,4 @@
- document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.5"; 
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.6"; 
 
 
 var circle = document.getElementById("id_circle");
@@ -15,8 +15,14 @@ function on_touch_move(e)
 
 	for (var i = 0; i < touches.length; i++){
 	var circle = touches[i].target;
+	if (touches[i].pageX - rect_svg.left <= rect_svg.width &&
+		touches[i].pageY - rect_svg.top  <= rect_svg.height &&
+		touches[i].pageX - rect_svg.left >=0 &&
+		touches[i].pageY - rect_svg.top >= 0
+		){
 	circle.setAttribute("cx" , touches[i].pageX - rect_svg.left);
 	circle.setAttribute("cy" , touches[i].pageY - rect_svg.top);
 	
+	}
 	}
 }
