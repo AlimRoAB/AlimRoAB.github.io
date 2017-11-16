be8d5092d22892ae8e86291c8af6ea7f06bfb6c4
AlimRoAB <kabaca.alim@gmail.com>
2017-11-16 05:03:17 -0800

    new

--- gyro.js
+++ gyro.js
@@ -1,15 +1,23 @@
 document.getElementById("id_business").innerHTML = "Buseiness level version: 2017.11.16.2";
 
 addEventListener("deviceorientation", on_device_orientation);
+addEventListener("devicemotion", on_device_motion);
 
 
 function on_device_orientation(e){
 
-	document.getElementById("z").innerHTML = "z = " + e.alpha;
-	document.getElementById("x").innerHTML = "x = " + e.beta;
-	document.getElementById("y").innerHTML = "y = " + e.gamma;
+	document.getElementById("z").innerHTML = "gyro z = " + e.alpha;
+	document.getElementById("x").innerHTML = "gyro x = " + e.beta;
+	document.getElementById("y").innerHTML = "gyro y = " + e.gamma;
 
 
 
 
 								 }
+	//-------------------------------
+function on_device_orientation(e){
+	
+	
+	
+	
+}
