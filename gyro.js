2b664c39f1220cf70b981a2b8e65ba228602e415
AlimRoAB <kabaca.alim@gmail.com>
2017-11-16 04:48:47 -0800

    uptade

--- gyro.js
+++ gyro.js
@@ -1,6 +1,6 @@
-document.getElementById("id_business").innerHTML = "Buseiness level version: 2017.11.16.0";
+document.getElementById("id_business").innerHTML = "Buseiness level version: 2017.11.16.1";
 
-addEventListener("deviceorientation0", on_device_orientation);
+addEventListener("deviceorientation", on_device_orientation);
 
 
 function on_device_orientation(e){
