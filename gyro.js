d6deadff35d269f9ba84f8166087077124f4a0c4
AlimRoAB <kabaca.alim@gmail.com>
2017-11-16 05:16:46 -0800

    accelerationIncludingGravity

--- gyro.js
+++ gyro.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business").innerHTML = "Buseiness level version: 2017.11.16.4";
+document.getElementById("id_business").innerHTML = "Buseiness level version: 2017.11.16.5";
 
 addEventListener("deviceorientation", on_device_orientation);
 addEventListener("devicemotion", on_device_motion);
@@ -20,6 +20,11 @@ function on_device_motion(e){
 	document.getElementById("acc_x").innerHTML = "acc_x = " + Math.round(e.acceleration.x * 100) / 100;
 	document.getElementById("acc_y").innerHTML = "acc_y = " + Math.round(e.acceleration.y * 100) / 100;
 	document.getElementById("acc_z").innerHTML = "acc_z = " + Math.round(e.acceleration.z * 100) / 100;
+	
+	
+	document.getElementById("accg_x").innerHTML = "accg_x = " + Math.round(e.accelerationIncludingGravity.x * 100) / 100;
+	document.getElementById("accg_y").innerHTML = "accg_y = " + Math.round(e.accelerationIncludingGravity.y * 100) / 100;
+	document.getElementById("accg_z").innerHTML = "accg_z = " + Math.round(e.accelerationIncludingGravity.z * 100) / 100;
 	
 	
 }
